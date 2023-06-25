5676dd38de7542b1537dc7f6a600ea1b03778f93
Git-Balance <davidbond001@icloud.com>
2023-06-24 19:39:42 -0700

    Lab is almost complete, minus a bug and commenting
    The lab is almost complete. However, there is one major bug and there is no documentation.
    The bug is caused by the deletion of dead hares. When a dead hare is deleted, the next hare takes its index in the ArrayList. Since the current deletion process relies on indexes, this causes some dead hares to not be removed from simulation.

--- Lynx.java
+++ Lynx.java
@@ -3,26 +3,29 @@ import java.util.Random;
 
 public class Lynx extends Organism {
     private boolean canReproduce;
-    
+
     public Lynx() {
         canReproduce = false;
     }
-    
+
     public void changeReproduce(boolean newState) { canReproduce = newState; };
+
     public boolean getReproduce() { return canReproduce; }
-    
+
     public void eatPrey(ArrayList<Hare> preys) {
         int eaten = 0;
         Random rand = new Random();
-        
+
         for (Hare prey : preys) {
-            int chanceEaten = rand.nextInt(10);
-            if (chanceEaten <= 1) {
-                prey.die();
-                eaten++;
+            if (prey.getAlive() == true) {
+                int chanceEaten = rand.nextInt(10);
+                if (chanceEaten <= 1) {
+                    prey.die();
+                    eaten++;
+                }
             }
         }
-        
+
         if (eaten >= 3) {
             changeReproduce(true);
         }
@@ -30,11 +33,11 @@ public class Lynx extends Organism {
             die();
         }
     }
-    
+
     public void reset() {
         changeReproduce(false);
     }
-    
+
     public String toString() {
         return "Lynx(" + getAge() + "," + getAlive() + "," + getReproduce() + ")";
     }
--- Simulation.java
+++ Simulation.java
@@ -1,26 +1,59 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Simulation {
 
     private final int HARE_RESET_SIZE = 3;
     private final int LYNX_RESET_SIZE = 1;
 
+    private ArrayList<Hare> hares = new ArrayList<Hare>();
+    private ArrayList<Lynx> lynxes = new ArrayList<Lynx>();
+    private ArrayList<String> events = new ArrayList<String>();
+    private int generations = 0;
+
     public static void main(String[] args) {
         Simulation simulation = new Simulation();
-        try
-        {
-            simulation.tests();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
+        Scanner in = new Scanner(System.in);
 
-    ArrayList<Hare> hares = new ArrayList<Hare>();
-    ArrayList<Lynx> lynxes = new ArrayList<Lynx>();
-    ArrayList<String> events = new ArrayList<String>();
-    int generations = 0;
+        final String line = "-------------------------";
+        final String instructions = "Enter what you want to do (n,d,t,q)\n> "; 
+        
+        simulation.displayInfo();
+        System.out.println(line);
+        System.out.print(instructions);
+        String input = in.next().toLowerCase();
+        if (!(input.equals("n"))) {
+            System.out.println(line);
+        }
+        while (!(input.equals("q"))) {
+            if (input.equals("n")) {
+                simulation.nextStep();
+            }
+            else if (input.equals("d")) {
+                simulation.displayDetails();
+            }
+            else if (input.equals("t")) {
+                try
+                {
+                    simulation.tests();
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            
+            System.out.println(line);
+            simulation.displayInfo();
+            System.out.println(line);
+            System.out.print(instructions);
+            input = in.next().toLowerCase();
+            if (!(input.equals("n"))) {
+                System.out.println(line);
+            }
+        }
+
+    }
 
     public Simulation() {
         hares.add(new Hare());
@@ -28,31 +61,36 @@ public class Simulation {
         hares.add(new Hare());
         lynxes.add(new Lynx());
     }
-    
+
     public void nextStep() {
+        // Prepare for the next generation
         generations++;
         events.clear();
+        for (Hare hare : hares) {
+            hare.age();
+        }
+        for (Lynx lynx : lynxes) {
+            lynx.age();
+        }
         if (hares.size() == 0) {
             for (int x = 0; x < HARE_RESET_SIZE; x++) {
                 hares.add(new Hare());
             }
-            events.add("Hare population reset to 3");
+            events.add("Hare population reset to 3 due to extinction");
         }
         if (lynxes.size() == 0) {
             for (int x = 0; x < LYNX_RESET_SIZE; x++){
                 lynxes.add(new Lynx());
             }
-            events.add("Lynx population reset to 1");
+            events.add("Lynx population reset to 1 due to extinction");
         }
-        
+
         // Make organisms make checks to see if they live or die
         for (Lynx lynx : lynxes) {
             lynx.eatPrey(hares);
         }
-        
+
         // Remove dead organisms
-        // TODO: this section's code doesn't work for obvious reasons
-        //       (writing to a list that is being read)
         for (int x = 0; x < hares.size(); x++) {
             if (!(hares.get(x).getAlive())) {
                 hares.remove(x);
@@ -63,7 +101,7 @@ public class Simulation {
                 lynxes.remove(x);
             }
         }
-        
+
         // Make organisms reproduce
         int oldHaresSize = hares.size();
         for (int x = 0; x < oldHaresSize; x++){
@@ -72,15 +110,33 @@ public class Simulation {
         int newLynxes = 0;
         for (Lynx lynx : lynxes) {
             if (lynx.getReproduce() == true) {
-                lynxes.add(new Lynx());
+                newLynxes++;
                 lynx.reset();
             }
         }
+        for (int x = 0; x < newLynxes; x++) {
+            lynxes.add(new Lynx());
+        }
     }
-    
+
     public void displayInfo() {
-        System.out.println("Generation " + generations + " (" + events.size() + " event)");
+        System.out.println("Generation " + generations + " (" + events.size() + " events)");
         System.out.println(hares.size() + " Hares | " + lynxes.size() + " Lynxes");
+    }
+
+    public void displayDetails() {
+        System.out.println("Generation: " + generations);
+        System.out.println("Events:");
+        for (String event : events) {
+            System.out.println("| " + event);
+        }
+        System.out.println("Organisms (age, alive, reproduce):");
+        for (Hare hare : hares) {
+            System.out.println("| " + hare);
+        }
+        for (Lynx lynx: lynxes) {
+            System.out.println("| " + lynx);
+        }
     }
 
     public void tests() throws Exception {
@@ -113,10 +169,12 @@ public class Simulation {
         int alive = 0;
         for (Hare prey : testPreys) { if (prey.getAlive()) { alive++; }}
         System.out.println(alive);
+
         Simulation testS = new Simulation();
         testS.nextStep();
         testS.nextStep();
         testS.nextStep();
         testS.displayInfo();
+        testS.displayDetails();
     }
 }
